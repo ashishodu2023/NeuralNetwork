e2cfe007aef4ecab4a3456575846f3b2ddf9c101
averma111 <ashishn8751@outlook.com>
2023-11-23 00:59:26 -0800

    Added PCA

--- DigitPrediction.py
+++ DigitPrediction.py
@@ -5,7 +5,8 @@ from OneHot import encode_labels
 import matplotlib.pyplot as plt
 import pandas as pd
 from Normalize import normalize_data
-
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+from sklearn.decomposition import PCA
 
 def train_nn(X, y, hidden_size, output_size, epochs, learning_rate):
     input_size = X.shape[1]
@@ -30,7 +31,7 @@ def train_nn(X, y, hidden_size, output_size, epochs, learning_rate):
     return weights_input_hidden, weights_hidden_output
 
 
-def plot_confusion_matrix(df_confusion, title='Confusion matrix', cmap='PuBuGn'):
+def plot_confusion_matrix(df_confusion, title='Confusion matrix', cmap='BuGn'):
     plt.matshow(df_confusion, cmap=cmap)
     plt.colorbar()
     tick_marks = np.arange(len(df_confusion.columns))
@@ -56,6 +57,54 @@ def get_confusion_matrix(y_train, y_pred):
     df_confusion = pd.crosstab(y, y_pred, rownames=['Actual'], colnames=['Predicted'], margins=True)
     return df_confusion
 
+def make_predictions_train_data(X_train_normalized,y_true, trained_weights_input_hidden, trained_weights_hidden_output):
+    # Perform predictions on training data
+    y_train_pred = predict(X_train_normalized, trained_weights_input_hidden, trained_weights_hidden_output)
+    # Get Confusion matrix
+    df_confusion = get_confusion_matrix(y_true, y_train_pred)
+    print("========================Confusion Matrix Traning Data========================\n")
+    print(df_confusion)
+    print()
+    print(f'The model train accuracy is ={accuracy_score(y_true, y_train_pred):.5f}\n')
+    print(f"The model train precision is ={precision_score(y_true, y_train_pred,average='weighted'):.5f}\n")
+    print(f"The model  train recall is ={recall_score(y_true, y_train_pred,average='weighted'):.5f}\n")
+
+
+
+def make_predictions_test_data(X_test_normalized,y_test, trained_weights_input_hidden, trained_weights_hidden_output):
+    # Perform predictions on test data
+
+    y_test_pred = predict(X_test_normalized, trained_weights_input_hidden, trained_weights_hidden_output)
+    # Get Confusion matrix
+    df_confusion = get_confusion_matrix(y_test, y_test_pred)
+    print("========================Confusion Matrix Test Data===========================\n")
+    print(df_confusion)
+    print()
+    print(f'The model test accuracy is ={accuracy_score(y_test, y_test_pred):.5f}\n')
+    print(f"The model test precision is ={precision_score(y_test, y_test_pred,average='weighted'):.5f}\n")
+    print(f"The model  test recall is ={recall_score(y_test, y_test_pred,average='weighted'):.5f}\n")
+
+def perform_pca(X_test_normalized):
+    pca = PCA(n_components = 0.9)
+    # perform PCA on the scaled data
+    pca.fit(X_test_normalized)
+    X_pca_test = pca.transform(X_test_normalized)
+    return X_pca_test
+
+
+def make_predictions_pca_data(X_test_normalized,y_test, trained_weights_input_hidden, trained_weights_hidden_output):
+    # Perform predictions on test data
+
+    y_test_pred = predict(X_test_normalized, trained_weights_input_hidden, trained_weights_hidden_output)
+    # Get Confusion matrix
+    df_confusion = get_confusion_matrix(y_test, y_test_pred)
+    print("========================Confusion Matrix PCA Data===========================\n")
+    print(df_confusion)
+    print()
+    print(f'The model test accuracy is ={accuracy_score(y_test, y_test_pred):.5f}\n')
+    print(f"The model test precision is ={precision_score(y_test, y_test_pred,average='weighted'):.5f}\n")
+    print(f"The model  test recall is ={recall_score(y_test, y_test_pred,average='weighted'):.5f}\n")
+
 
 if __name__ == '__main__':
     # Load npy data from train and test data.
@@ -63,26 +112,29 @@ if __name__ == '__main__':
     y_true = np.load('train/y_train.npy')
     # One-hot encode the labels
     y_one_hot = encode_labels(y_true)
-    X_normalized = normalize_data(X)
+    X_train_normalized = normalize_data(X)
 
     # Train the neural network
-    hidden_size = 8  # Increased hidden size
+    hidden_size = 16  # Increased hidden size
     output_size = y_one_hot.shape[1]
-    epochs = 1000  # Increased number of epochs
+    epochs = 3000  # Increased number of epochs
     learning_rate = 0.001  # Adjusted learning rate
 
-    trained_weights_input_hidden, trained_weights_hidden_output = train_nn(X_normalized, y_one_hot,
+    trained_weights_input_hidden, trained_weights_hidden_output = train_nn(X_train_normalized, y_one_hot,
                                                                            hidden_size, output_size, epochs,
                                                                            learning_rate)
-    # Perform predictions
-    print("True Labels:", y_true)
-    X_test_normalize = normalize_data(np.load('test/X_test.npy'))
-    y_pred_label = predict(X_test_normalize, trained_weights_input_hidden, trained_weights_hidden_output)
-    print("Predicted Labels:", y_pred_label)
+    make_predictions_train_data(X_train_normalized,y_true, trained_weights_input_hidden, trained_weights_hidden_output)
+    print()
+    X_test_normalized = normalize_data(np.load('test/X_test.npy'))
+    y_test_true = np.load('test/y_test.npy')
+    make_predictions_test_data(X_test_normalized,y_test_true, trained_weights_input_hidden, trained_weights_hidden_output)
+    print()
+    X_pca_test=perform_pca(X_test_normalized)
+    y_one_hot_test = encode_labels(y_test_true)
+    trained_weights_input_hidden_pca, trained_weights_hidden_output_pca = train_nn(X_pca_test, y_one_hot_test,
+                                                                           hidden_size, output_size, epochs,
+                                                                           learning_rate)
+    make_predictions_test_data(X_pca_test, y_test_true, trained_weights_input_hidden_pca, trained_weights_hidden_output_pca)
 
-    # Get Confusion matrix
-    df_confusion = get_confusion_matrix(y_true, y_pred_label)
-    print(df_confusion)
 
-    # Plot confusion matrix
-    plot_confusion_matrix(df_confusion)
+
--- temp.py
+++ temp.py
@@ -1,136 +1,49 @@
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+with open('file.txt','r') as file_obj:
+    binary_sequences_with_labels = file_obj.readlines()
+# Split the input into sequences and labels
+binary_sequences_with_labels_dict = dict()
+binary_sequences_with_labels_list =[]
+i = 0
+# iterating until the end of the file
+while i < len(binary_sequences_with_labels):
+    # extracting 32 lines (image matrix)
+    data_lines_list = []
+    for j in range(i, i + 32):
+        # reading each line as string from i to i+32 to get the image matrix lines
+        single_line = binary_sequences_with_labels[j].strip()
+        # add these string lines into a temp list
+        data_lines_list.append(list(map(int, single_line)))  # converting each character to int
 
-def sigmoid_derivative(x):
-    return x * (1 - x)
+    # extracting the label from the 33rd line
+    label = int(binary_sequences_with_labels[j + 1].strip())
 
-def softmax(x):
-    exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
-    return exp_values / np.sum(exp_values, axis=1, keepdims=True)
+    # concatenate the list of lines to form a matrix
+    data_matrix = np.array(data_lines_list).flatten()
 
-def initialize_weights(input_size, hidden_size, output_size):
-    np.random.seed(42)
-    weights_input_hidden = np.random.randn(input_size, hidden_size)
-    weights_hidden_output = np.random.randn(hidden_size, output_size)
-    return weights_input_hidden, weights_hidden_output
+    # append label and data matrix to the list
+    binary_sequences_with_labels_list.append([label, data_matrix])
 
-def forward_propagation(X, weights_input_hidden, weights_hidden_output):
-    hidden_layer_input = np.dot(X, weights_input_hidden)
-    hidden_layer_output = sigmoid(hidden_layer_input)
+    # move to the next block of data for the image matrix and its label
+    i += 33
 
-    output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
-    output_layer_output = softmax(output_layer_input)
+labels, data_matrix = zip(*binary_sequences_with_labels_list)
+X = np.array(data_matrix)
+y= np.array(labels)
 
-    return hidden_layer_output, output_layer_output
+#np.save("train/X.npy")
+#np.save('test/y.npy')
 
-def backward_propagation(X, y, hidden_layer_output, output_layer_output, weights_hidden_output):
-    output_error = output_layer_output - y
-    output_delta = output_error
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+# Save NumPy arrays to .npy files
+np.save('train/X_train.npy', X_train)
+np.save('test/X_test.npy', X_test)
 
-    hidden_error = output_delta.dot(weights_hidden_output.T)
-    hidden_delta = hidden_error * sigmoid_derivative(hidden_layer_output)
-
-    return hidden_delta, output_delta
-
-def update_weights(X, hidden_layer_output, output_delta, hidden_delta, weights_input_hidden, weights_hidden_output, learning_rate, clip_value=1.0):
-    grad_output_weights = hidden_layer_output.T.dot(output_delta)
-    grad_input_weights = X.T.dot(hidden_delta)
-
-    # Gradient Clipping
-    grad_output_weights = np.clip(grad_output_weights, -clip_value, clip_value)
-    grad_input_weights = np.clip(grad_input_weights, -clip_value, clip_value)
-
-    weights_hidden_output -= grad_output_weights * learning_rate
-    weights_input_hidden -= grad_input_weights * learning_rate
-
-    return weights_input_hidden, weights_hidden_output
-
-
-def normalize_data(X):
-    mean = np.mean(X, axis=0)
-    std = np.std(X, axis=0)
-
-    # Check for zero standard deviation
-    std[std == 0] = 1.0
-
-    # Check for NaN values in the input data
-    if np.isnan(X).any():
-        raise ValueError("Input data contains NaN values.")
-
-    normalized_X = (X - mean) / std
-
-    return normalized_X
+# Save labels as well
+np.save('train/y_train.npy', np.array(y_train))
+np.save('test/y_test.npy', np.array(y_test))
 
 
-def encode_labels(y):
-    num_classes = len(np.unique(y))
-    encoded_labels = np.eye(num_classes)[y]
-    return encoded_labels
-
-def train_neural_network(X, y, hidden_size, output_size, epochs, learning_rate):
-    input_size = X.shape[1]
-    weights_input_hidden, weights_hidden_output = initialize_weights(input_size, hidden_size, output_size)
-
-    for epoch in range(epochs):
-        # Forward Propagation
-        hidden_layer_output, output_layer_output = forward_propagation(X, weights_input_hidden, weights_hidden_output)
-
-        # Backward Propagation
-        hidden_delta, output_delta = backward_propagation(X, y, hidden_layer_output, output_layer_output, weights_hidden_output)
-
-        # Update Weights
-        weights_input_hidden, weights_hidden_output = update_weights(X, hidden_layer_output, output_delta, hidden_delta, weights_input_hidden, weights_hidden_output, learning_rate)
-
-        # Print loss every 100 epochs
-        if epoch % 100 == 0:
-            loss = cross_entropy_loss(y, output_layer_output)
-            print(f"Epoch {epoch}, Loss: {loss}")
-
-    return weights_input_hidden, weights_hidden_output
-def cross_entropy_loss(y_true, y_pred):
-    epsilon = 1e-15
-    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-    m = y_true.shape[0]
-    loss = -np.sum(y_true * np.log(y_pred + epsilon)) / m
-    return loss
-
-# Example usage:
-# Assuming 'X' is your input data and 'label' is your multilabel output data (between 0 and 9)
-# Adjust 'hidden_size', 'output_size', 'epochs', and 'learning_rate' based on your requirements
-
-# Generate some example data
-X = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]])
-
-label = np.array([0, 1, 2, 3, 4])  # Labels between 0 and 9
-
-# Normalize the input data
-X_normalized = normalize_data(X)
-
-# One-hot encode the labels
-y_one_hot = encode_labels(label)
-
-# Train the neural network
-hidden_size = 8  # Increased hidden size
-output_size = y_one_hot.shape[1]
-epochs = 3000  # Increased number of epochs
-learning_rate = 0.0001  # Adjusted learning rate
-
-trained_weights_input_hidden, trained_weights_hidden_output = train_neural_network(X_normalized, y_one_hot, hidden_size, output_size, epochs, learning_rate)
-
-print("NaN values in X:", np.isnan(X).any())
-print("Infinite values in X:", np.isinf(X).any())
-print("NaN values in y_one_hot:", np.isnan(y_one_hot).any())
-print("Infinite values in y_one_hot:", np.isinf(y_one_hot).any())
-
-
-# Perform predictions
-_, predictions = forward_propagation(X_normalized, trained_weights_input_hidden, trained_weights_hidden_output)
-predicted_labels = np.argmax(predictions, axis=1)
-print("True Labels:", label)
-print("Predicted Labels:", predicted_labels)
